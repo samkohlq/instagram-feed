7548fdeb01529996996f9cd5b0e3077b95353586
Samantha Koh <26067359+samkohlq@users.noreply.github.com>
2020-03-14 22:06:17 +0800

    fix max-width of Post component to 400px

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,13 +1,13 @@
 import { Card, CardContent, CardMedia, Typography } from "@material-ui/core";
-import React from "react";
 import * as firebase from "firebase";
+import React from "react";
 
 const Post = props => {
   const username = firebase.auth().currentUser.displayName;
   const { src } = props;
   return (
     <div>
-      <Card style={{ margin: 4 }}>
+      <Card style={{ maxWidth: "400px", margin: 4 }}>
         <CardMedia component="img" alt="some image" width="180" image={src} />
         <CardContent>
           <Typography variant="body2" color="textSecondary" component="p">
